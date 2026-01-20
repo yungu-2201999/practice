9c8ba4bc3ad5a8d310058473425f5345b4728874
yungu-2201999 <rguo1022@gmail.com>
2026-01-20 23:52:24 +0800

    feat: add ts

--- 08TS/index.ts
+++ 08TS/index.ts
@@ -17,3 +17,9 @@ function fn(param1:string,param2:number):void
 function fn(param1:any, param2?:any):void{
     console.log(param1)
 }
+
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish;// type A = number
+ 
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish;//type M = string | number
