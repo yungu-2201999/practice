b1f68da0b7b293f8a563faf5d515705de70c87ce
yungu-2201999 <rguo1022@gmail.com>
2026-01-25 11:11:50 +0800

    feat: taro

---
+++ 09移动端/myFirstTaroDemo/src/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list:[]
+  }
+})
